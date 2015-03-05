3b57620a4ca6b5683361e207bc761fafbcde581e
Ram Iyer <ramky.iyer@yahoo.com>
2015-03-05 15:43:25 -0800

    using single require

--- bin/setup.rb
+++ bin/setup.rb
@@ -5,6 +5,4 @@ require 'yaml'
 require 'mongo_mapper'
 require 'pry'
 
-Dir['lib/initializers/*.rb'].each { |file| require_relative '../' + file   }
-Dir['lib/amazon/*.rb'].each { |file| require_relative '../' + file   }
-Dir['lib/*.rb'].each { |file| require_relative '../' + file   }
+Dir['lib/**/*.rb'].each { |file| require_relative '../' + file  }
